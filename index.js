67226a2777f0ba32ad961b5de483e36d778b5d98
Praveen Baggiri <102279747+praveen284@users.noreply.github.com>
2022-06-14 20:07:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -73,65 +73,69 @@ function reset() {
 
 // for Music
 
-// buttonpress
-for (let i = 0; i < document.getElementsByClassName("drum").length; i++) {
-    document.getElementsByTagName("button")[i].addEventListener("click", function () {
-        let buttoninnerHTML = this.innerHTML;
-        makeSound(buttoninnerHTML);
-        buttonAnimation(this.innerHTML);
-    });
-}
+// for Music
 
-// keyboardpress
-
-document.addEventListener("keypress", function (event) {
-    makeSound(event.key);
-    buttonAnimation(event.key);
-});
-
-function makeSound(key) {
-    switch (key) {
-        case "a":
-            let tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();
-            break;
-        case "s":
-            let tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play();
-            break;
-        case "d":
-            let tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();
-            break;
-        case "f":
-            let tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();
-            break;
-        case "j":
-            let crash = new Audio("sounds/crash.mp3");
-            crash.play();
-            break;
-        case "k":
-            let kick = new Audio("sounds/kick-bass.mp3");
-            kick.play();
-            break;
-        case "l":
-            let snare = new Audio("sounds/snare.mp3");
-            snare.play();
-            break;
-        default:
-            console.log(buttoninnerHTML);
+function success() {
+    // buttonpress
+    for (let i = 0; i < document.getElementsByClassName("drum").length; i++) {
+        document.getElementsByTagName("button")[i].addEventListener("click", function () {
+            let buttoninnerHTML = this.innerHTML;
+            makeSound(buttoninnerHTML);
+            buttonAnimation(this.innerHTML);
+        });
     }
+
+
+    // keyboardpress
+    document.addEventListener("keypress", function (event) {
+        makeSound(event.key);
+        buttonAnimation(event.key);
+    });
+
+    function makeSound(key) {
+        switch (key) {
+            case "a":
+                let tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "s":
+                let tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "d":
+                let tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "f":
+                let tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "j":
+                let crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+            case "k":
+                let kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+            case "l":
+                let snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+            default:
+                console.log(buttoninnerHTML);
+        }
+    }
+
+    function buttonAnimation(currentKey) {
+        let activeButton = document.querySelector("." + currentKey);
+        activeButton.classList.add("pressed");
+
+        setTimeout(function () {
+            activeButton.classList.remove("pressed");
+        }, 300)
+    }
+
 }
-
-function buttonAnimation(currentKey) {
-    let activeButton = document.querySelector("." + currentKey);
-    activeButton.classList.add("pressed");
-
-    setTimeout(function () {
-        activeButton.classList.remove("pressed");
-    }, 300)
-}
-
 
 
